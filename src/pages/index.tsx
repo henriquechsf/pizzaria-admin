f6895ef627ae06c9971804524e0e26addbc502c7
Carlos Henrique S. Ferreira <carlos.ferreira@gazin.com.br>
2022-09-19 11:15:53 -0300

    feat: send form data to sign in function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,27 @@ import Image from "next/image";
 import { Input } from "../components/ui/Input";
 import { Button } from "../components/ui/Button";
 import Link from "next/link";
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (event: FormEvent) => {
+    event.preventDefault();
+
+    let data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  };
+
   return (
     <>
       <Head>
@@ -16,11 +35,21 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Pizzaria" />
         <div className={styles.login}>
-          <form>
-            <Input type="text" placeholder="Digite seu e-mail" />
-            <Input type="password" placeholder="Sua senha" />
+          <form onSubmit={handleLogin}>
+            <Input
+              type="text"
+              placeholder="Digite seu e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="Sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-            <Button type="submit" loading={true}>
+            <Button type="submit" loading={false}>
               Acessar
             </Button>
           </form>
