55d1eb43816f2f40f74b72702fd12e4a9c138d7b
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-09-19 00:16:26 -0300

    feat: create input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from 'next/head';
+import styles from '../../styles/home.module.scss';
+
+import logoImg from '../../public/logo.svg';
+import Image from 'next/image';
+import { Input } from '../components/ui/Input';
 
 export default function Home() {
   return (
@@ -6,8 +11,15 @@ export default function Home() {
       <Head>
         <title>Pizzaria - Faca seu login</title>
       </Head>
-      <div>
-        <h1>Sujeito Pizzaria</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Pizzaria" />
+      </div>
+
+      <div className={styles.login}>
+        <form>
+          <Input type="text" placeholder='Digite seu e-mail' />
+          <Input type="password" placeholder='Sua senha' />
+        </form>
       </div>
     </>
   )
