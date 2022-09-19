a56cd86ba6b8054ed2962981bef29c6c611a1cd7
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-09-19 18:08:31 -0300

    feat: SSR redirect if user is logged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { toast } from "react-toastify";
+import { canSSRGuest } from "../utils/canSSRGuest";
 
 export default function Home() {
   const { signIn } = useContext(AuthContext);
@@ -72,3 +73,9 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  };
+});
---
+++ src/utils/canSSRGuest.ts
@@ -0,0 +1,26 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+// funcao para paginas que soh pode ser acessadas por visitantes
+export function canSSRGuest<T>(fn: GetServerSideProps<T>) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<T>> => {
+    const cookies = parseCookies(ctx);
+    // se tentar acessar caso ja esteja logado
+    if (cookies["@nextauth.token"]) {
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
